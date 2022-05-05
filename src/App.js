faec48caf91e748e763d4b1139d03b02f1e6e3ff
Kieran O'Day <kieran.oday@gmail.com>
2022-05-05 12:02:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [userId, setUserID] = useState('')
+  const [amount, setAmount] = useState('')
+  const [userArray, setUserArray] = useState([])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          axios.get("https://randomuser.me/api/").then((response) => {
+            console.log(response)
+            const results = response.data.results
+            const user = results && results.length > 0 ? results[0] : null
+            if (user) {
+              const newUser = {
+                id: userId,
+                amount: amount,
+                email: user.email,
+                firstName: user.name.first,
+                lastName: user.name.last
+              }
+              setUserArray(oldUserArray => [...oldUserArray, newUser])
+              setAmount('')
+              setUserID('')
+            }else{
+              alert('something went wrong')
+            }
+          })
+        }}
         >
-          Learn React
-        </a>
+          <p>
+            <input value={userId} onChange={(event) => setUserID(event.target.value)} placeholder='User ID' />
+          </p>
+          <p>
+            <input value={amount} onChange={(event) => setAmount(event.target.value)} placeholder='Amount' />
+          </p>
+          <p>
+            <button type='submit'>Submit</button>
+          </p>
+        </form>
+
+        <div>
+          {userArray.map((user) => {
+            return(
+              <div>
+                <p>Name: {user.firstName} {user.lastName}</p>
+                <p>Email: {user.email}</p>
+                <p>ID: {user.id}</p>
+                <p>Amount: {user.amount}</p>
+              </div>
+            )
+          })}
+        </div>
       </header>
     </div>
   );
